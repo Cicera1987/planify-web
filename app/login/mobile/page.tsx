4e0322deaf8d02873c84c8f54f067ca521aec327
Cicera Ribeiro <maccicera@gmail.com>
2025-09-18 15:25:43 -0300

    fix: ajuste na responsividade tela login

--- app/login/mobile/page.tsx
+++ app/login/mobile/page.tsx
@@ -1,26 +1,28 @@
 "use client";
 import React from "react";
 import LoginForm from "@/app/components/forms/formLogin";
-import "./styles.css";
 import { useAuth } from "@/app/hooks/useAuth";
 
 export default function MobileLogin() {
   const { login } = useAuth();
 
   return (
-    <div className="mobile-login">
-      <div className="header-mobile">
-        <img
-          src="/images/logo.png"
-          alt="Logo Planify"
-          className="logo-mobile"
-        />
-        <h1 className="app-title-mobile">Planify</h1>
-      </div>
+    <div className="flex flex-col items-center justify-center min-h-screen bg-white">
+    
+      <img
+        src="/images/logo.png"
+        alt="Logo Planify"
+        className="w-24 sm:w-28 mb-4"
+      />
+      <h1 style={{ color: "var(--primary-3)" }} className="text-2xl sm:text-3xl font-bold mb-8">
+        Planify
+      </h1>
 
-      <div className="form-container-mobile">
+      <div className="w-full max-w-md">
         <LoginForm onSubmit={(data) => login(data.email, data.password)} />
       </div>
     </div>
+
+
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,42 @@ module.exports = {
   ],
   theme: {
     extend: {
+      spacing: {
+        "space-4": "var(--space-4px)",    // 0.25rem
+        "space-8": "var(--space-8px)",    // 0.5rem
+        "space-12": "var(--space-12px)",  // 0.75rem
+        "space-16": "var(--space-16px)",  // 1rem
+        "space-20": "var(--space-20px)",  // 1.25rem
+        "space-24": "var(--space-24px)",  // 1.5rem
+        "space-28": "var(--space-28px)",  // 1.75rem
+        "space-32": "var(--space-32px)",  // 2rem
+        "space-36": "var(--space-36px)",  // 2.25rem
+        "space-40": "var(--space-40px)",  // 2.5rem
+        "space-44": "var(--space-44px)",  // 2.75rem
+        "space-48": "var(--space-48px)",  // 3rem
+        "space-52": "var(--space-52px)",  // 3.25rem
+        "space-56": "var(--space-56px)",  // 3.5rem
+        "space-60": "var(--space-60px)",  // 3.75rem
+        "space-64": "var(--space-64px)",  // 4rem
+        "space-68": "var(--space-68px)",  // 4.25rem
+        "space-72": "var(--space-72px)",  // 4.5rem
+        "space-76": "var(--space-76px)",  // 4.75rem
+        "space-80": "var(--space-80px)",  // 5rem
+        "space-84": "var(--space-84px)",  // 5.25rem
+        "space-88": "var(--space-88px)",  // 5.5rem
+        "space-92": "var(--space-92px)",  // 5.75rem
+        "space-96": "var(--space-96px)",  // 6rem
+        "space-100": "var(--space-100px)", // 6.25rem
+        "space-104": "var(--space-104px)", // 6.5rem
+        "space-108": "var(--space-108px)", // 6.75rem
+        "space-112": "var(--space-112px)", // 7rem
+        "space-116": "var(--space-116px)", // 7.25rem
+        "space-120": "var(--space-120px)", // 7.5rem
+        "space-124": "var(--space-124px)", // 7.75rem
+        "space-128": "var(--space-128px)", // 8rem
+        "space-297": "var(--space-297px)", // 18.5625rem
+        "space-312": "var(--space-312px)", // 19.5rem
+      },
       fontFamily: {
         poppins: ["Poppins", "sans-serif"],
         roboto: ["Roboto", "sans-serif"],
@@ -14,34 +50,50 @@ module.exports = {
         montserratMono: ["Montserrat Subrayada", "sans-serif"],
         montserratAlternates: ["Montserrat Alternates", "sans-serif"],
       },
-      colors: {
-        primary_1: "#C0E0FF",
-        primary_2: "#198CFF",
-        primary_3: "#0063C7",
-        primary_4: "#003D7A",
-        secondary_1: "#24FEC1",
-        secondary_2: "#00D6E1",
-        secondary_3: "#00C4FF",
-        secondary_4: "#8F98FF",
-        neutral_1: "#424B5A",
-        neutral_2: "#B5BDC7",
-        neutral_3: "#939393",
-        neutral_4: "#EDEDED",
-        neutral_light: "#CECECE",
-        neutral_dark: "#2D2D2D",
-        neutral_danger: "#F33060",
-        success: "#0CE07A",
-        error: "#FF0000",
-        warning: "#FFD211",
-        black: "#000000",
-        white: "#FFFFFF",
-        background: "#B5BDC7",
-
-        background_color_light: rgba(224, 242, 254, 0.9),
-        background_color_green: rgba(31, 244, 173, 0.2),
-        background_color_blue_light: rgba(28, 140, 252, 0.2),
-        background_color_blue: rgba(2, 132, 199, 0.9),
+      fontWeight: {
+        normal: "var(--font-w-normal)",
+        medium: "var(--font-w-medium)",
+        semibold: "var(--font-w-semibold)",
+        bold: "var(--font-w-bold)",
       },
+      colors: {
+        primary: {
+          1: "var(--primary-1)",
+          2: "var(--primary-2)",
+          3: "var(--primary-3)",
+          4: "var(--primary-4)",
+        },
+        secondary: {
+          1: "var(--secondary-1)",
+          2: "var(--secondary-2)",
+          3: "var(--secondary-3)",
+          4: "var(--secondary-4)",
+        },
+        neutral: {
+          1: "var(--neutral-1)",
+          2: "var(--neutral-2)",
+          3: "var(--neutral-3)",
+          4: "var(--neutral-4)",
+          5: "var(--neutral-5)",
+          6: "var(--neutral-6)",
+          light: "var(--neutral-light)",
+          dark: "var(--neutral-dark)",
+          danger: "var(--neutral-danger)",
+          white: "var(--neutral-white)",
+        },
+        success: "var(--success)",
+        error: "var(--error)",
+        warning: "var(--warning)",
+        black: "var(--black)",
+        white: "var(--white)",
+        background: {
+          background: "var(--background-primary)",
+          light: "var(--background-color-light)",
+          green: "var(--background-color-green)",
+          blue_light: "var(--background-color-blue-light)",
+          blue: "var(--background-color-blue)",
+        },
+      }
     },
   },
   plugins: [],
