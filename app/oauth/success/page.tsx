b345753c9a6ffeb84b52b1023b648fad18105baf
Cicera1987 <91414413+Cicera1987@users.noreply.github.com>
2025-10-31 21:25:07 -0300

    feat: ajuate no botão google

--- app/oauth/success/page.tsx
+++ app/oauth/success/page.tsx
@@ -1,8 +1,22 @@
-import OAuthSuccess from "../success";
+"use client";
 
-export default function Success() {
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
 
-  return(
-    <OAuthSuccess/>
-  )
+export default function OAuthSuccess() {
+  const router = useRouter();
+
+  useEffect(() => {
+    const params = new URLSearchParams(window.location.search);
+    const token = params.get("token");
+
+    if (token) {
+      localStorage.setItem("@planify/token", token.replace("Bearer ", ""));
+      router.push("/home");
+    } else {
+      router.push("/login");
+    }
+  }, [router]);
+
+  return <div>Autenticando, aguarde...</div>;
 }
